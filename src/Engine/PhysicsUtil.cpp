7566ce648a740b431e0013be10dafe03cea9b5a5
Da-Huntha <Da-Huntha@lostmoment.com>
2013-01-28 23:03:36 +0100

    Feat 19: Add util functions
    - Quaternion transform functions
    - Deg to rad and vice versa for irr vectors

--- src/Engine/PhysicsUtil.cpp
+++ src/Engine/PhysicsUtil.cpp
@@ -14,3 +14,23 @@ irr::video::SColorf Engine::transformBulletColor(const btVector3& color)
 {
 	return irr::video::SColorf(color.getX(), color.getY(), color.getZ());
 }
+
+btQuaternion Engine::transformIrrQuaternion( const irr::core::quaternion& quaternion )
+{
+	return btQuaternion(quaternion.X, quaternion.Y, quaternion.Z, quaternion.W);
+}
+
+irr::core::quaternion Engine::transformBulletQuaternion( const btQuaternion& quaternion )
+{
+	return irr::core::quaternion(quaternion.getX(), quaternion.getY(), quaternion.getZ(), quaternion.getW());
+}
+
+irr::core::vector3df Engine::vecRadToDeg(const irr::core::vector3df& vector )
+{
+	return irr::core::vector3df(irr::core::radToDeg(vector.X), irr::core::radToDeg(vector.Y), irr::core::radToDeg(vector.Z));
+}
+
+irr::core::vector3df Engine::vecDegToRad(const irr::core::vector3df& vector )
+{
+	return irr::core::vector3df(irr::core::degToRad(vector.X), irr::core::degToRad(vector.Y), irr::core::degToRad(vector.Z));
+}
--- src/Engine/PhysicsUtil.h
+++ src/Engine/PhysicsUtil.h
@@ -1,13 +1,21 @@
 #pragma once
 
-#include <btBulletDynamicsCommon.h>
+#include <Bullet/LinearMath/btVector3.h>
+#include <Bullet/LinearMath/btQuaternion.h>
 #include <vector3d.h>
+#include <quaternion.h>
 #include <SColor.h>
 
 namespace Engine {
 
 	btVector3 transformIrrVector(const irr::core::vector3df& vector);
 	irr::core::vector3df transformBulletVector(const btVector3& vector);
+
+	btQuaternion transformIrrQuaternion(const irr::core::quaternion& quaternion);
+	irr::core::quaternion transformBulletQuaternion(const btQuaternion& quaternion);
+
+	irr::core::vector3df vecRadToDeg(const irr::core::vector3df& vector);
+	irr::core::vector3df vecDegToRad(const irr::core::vector3df& vector);
 
 	irr::video::SColorf transformBulletColor(const btVector3& color);
 }
