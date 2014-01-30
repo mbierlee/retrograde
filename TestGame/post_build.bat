f60dd44d88414fc9c815fe0216065d8c2d90926d
Mike Bierlee <mike.bierlee@lostmoment.com>
2014-01-30 01:23:28 +0100

    Feat 53: Setup release build configurations

--- TestGame/post_build.bat
+++ TestGame/post_build.bat
@@ -1,2 +1,2 @@
 rem Copy Irrlicht DLL
-xcopy /Y "%RETROGRADE_HOME%\lib\Irrlicht\x86\Irrlicht.dll" "%RETROGRADE_HOME%\TestGame\Debug Win32\"
+xcopy /Y "%RETROGRADE_HOME%\lib\Irrlicht\x86\Irrlicht.dll" "%RETROGRADE_HOME%\TestGame\Release Win32\"
---
+++ TestGame/post_build_debug.bat
@@ -0,0 +1,2 @@
+rem Copy Irrlicht DLL
+xcopy /Y "%RETROGRADE_HOME%\lib\Irrlicht\x86\Irrlicht.dll" "%RETROGRADE_HOME%\TestGame\Debug Win32\"
