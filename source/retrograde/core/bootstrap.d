6fdb632287c9ca224c9ecb66df5109dee17ec817
Mike Bierlee <m.bierlee@lostmoment.com>
2021-01-04 20:33:53 +0300

    Asdd bootstrap function to easily set-up and start game

---
+++ source/retrograde/core/bootstrap.d
@@ -0,0 +1,58 @@
+module retrograde.core.bootstrap;
+
+import retrograde.core.runtime;
+import retrograde.core.game;
+
+import poodinis;
+
+/**
+ * Bootstrap function for quickly setting up the DI framework with all required components.
+ * After set-up, the game is started.
+ */
+public void startGame(GameType : Game, EngineRuntimeType:
+        EngineRuntime = StandardEngineRuntime)(
+        shared DependencyContainer dependencies = new shared DependencyContainer())
+{
+
+    dependencies.register!(EngineRuntime, EngineRuntimeType);
+    dependencies.register!(Game, GameType);
+    auto runtime = dependencies.resolve!EngineRuntime;
+    runtime.startGame();
+}
+
+version (unittest)
+{
+    class TestGame : Game
+    {
+        public bool isInitialized;
+
+        @Autowire private EngineRuntime runtime;
+
+        public override void initialize()
+        {
+            isInitialized = true;
+        }
+
+        public override void update()
+        {
+            runtime.terminate();
+        }
+
+        public override void render(double extraPolation)
+        {
+        }
+
+        public override void cleanup()
+        {
+        }
+    }
+
+    @("Bootstrap of testgame")
+    unittest
+    {
+        auto dependencies = new shared DependencyContainer();
+        startGame!TestGame(dependencies);
+        const game = dependencies.resolve!TestGame;
+        assert(game.isInitialized);
+    }
+}
