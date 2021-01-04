8fb434b0f9f9a2b8c64affa62536dba59849f6db
Mike Bierlee <m.bierlee@lostmoment.com>
2021-01-04 05:02:59 +0300

    Initialize skeleton DUB project

---
+++ source/retrograde/test/sanity.d
@@ -0,0 +1,25 @@
+/**
+ * Retrograde Engine
+ *
+ * Authors:
+ *  Mike Bierlee, m.bierlee@lostmoment.com
+ * Copyright: 2014-2021 Mike Bierlee
+ * License:
+ *  This software is licensed under the terms of the MIT license.
+ *  The full terms of the license can be found in the LICENSE.txt file.
+ */
+
+module source.retrograde.test.sanity;
+
+version(unittest) {
+
+// Basic truthy tests to make sure the test set-up is ok.
+@("Truthy")
+unittest {
+    assert(true == true);
+    assert(1 == 1);
+    assert(2 > 1);
+    // assert(true == false); // Enable to test whether the testrunner trips
+}
+
+}
