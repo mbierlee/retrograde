07ffe745497e0ee7695dcbb44aaf98a25bcc70d4
Mike Bierlee <m.bierlee@lostmoment.com>
2021-04-30 01:56:17 +0300

    Add logging to core package

--- source/retrograde/core/package.d
+++ source/retrograde/core/package.d
@@ -3,6 +3,7 @@ module retrograde.core;
 public import retrograde.core.communication;
 public import retrograde.core.entity;
 public import retrograde.core.game;
+public import retrograde.core.logging;
 public import retrograde.core.platform;
 public import retrograde.core.runtime;
 public import retrograde.core.stringid;
