182128e7936f7cda8049d19dbea7de21d4017945
Mike Bierlee <m.bierlee@lostmoment.com>
2021-07-13 00:13:12 +0300

    Add missing public imports to package module

--- source/retrograde/core/package.d
+++ source/retrograde/core/package.d
@@ -1,9 +1,11 @@
 module retrograde.core;
 
-public import retrograde.core.messaging;
+public import retrograde.core.collections;
 public import retrograde.core.entity;
 public import retrograde.core.game;
+public import retrograde.core.input;
 public import retrograde.core.logging;
+public import retrograde.core.messaging;
 public import retrograde.core.platform;
 public import retrograde.core.runtime;
 public import retrograde.core.stringid;
