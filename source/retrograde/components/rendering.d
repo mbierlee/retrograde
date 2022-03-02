cd01f12e250add95163abaaac9df4b2ba0edcc0f
Mike Bierlee <m.bierlee@lostmoment.com>
2022-03-02 23:58:32 +0300

    Add camera component

--- source/retrograde/components/rendering.d
+++ source/retrograde/components/rendering.d
@@ -26,3 +26,24 @@ class RenderableComponent : EntityComponent {
 class DefaultShaderProgramComponent : EntityComponent {
     mixin EntityComponentIdentity!"DefaultShaderProgramComponent";
 }
+
+/**
+ * Entities with the CameraComponent are considered cameras.
+ *
+ * Together with a Position3DComponent and Orientation3DComponent they will determine
+ * what the user will see.
+ */
+class CameraComponent : EntityComponent {
+    mixin EntityComponentIdentity!"CameraComponent";
+}
+
+/**
+ * Entities with an ActiveCameraComponent are considered active and
+ * will be the ones used to render the view.
+ *
+ * Typically there is only one camera active. Entities still need to have a CameraComponent 
+ * as well or else they are not considered to be cameras.
+ */
+class ActiveCameraComponent : EntityComponent {
+    mixin EntityComponentIdentity!"ActiveCameraComponent";
+}
