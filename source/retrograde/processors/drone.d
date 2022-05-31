da7b80d353c804fbc87d254dda4c52458ab6ec1c
Mike Bierlee <m.bierlee@lostmoment.com>
2022-06-01 02:00:57 +0300

    Add rotation to drone

--- source/retrograde/processors/drone.d
+++ source/retrograde/processors/drone.d
@@ -14,11 +14,13 @@ module retrograde.processors.drone;
 import retrograde.core.entity : Entity, EntityProcessor;
 import retrograde.core.stringid : sid;
 import retrograde.core.messaging : MessageHandler, MagnitudeMessage;
-import retrograde.core.math : Vector3D, scalar;
+import retrograde.core.math : Vector3D, scalar, QuaternionD;
 
 import retrograde.components.drone : DroneControllableComponent;
 import retrograde.components.animation : TranslatingComponent, SpinningComponent;
 import retrograde.components.geometry : Orientation3DComponent, Position3DComponent;
+
+import std.math : PI;
 
 const auto droneChannel = sid("drone_channel");
 
@@ -28,6 +30,13 @@ const auto cmdDroneMoveLeft = sid("cmd_drone_move_left");
 const auto cmdDroneMoveRight = sid("cmd_drone_move_right");
 const auto cmdDroneMoveForwards = sid("cmd_drone_move_forwards");
 const auto cmdDroneMoveBackwards = sid("cmd_drone_move_backwards");
+
+const auto cmdDroneYawLeft = sid("cmd_drone_yaw_left");
+const auto cmdDroneYawRight = sid("cmd_drone_yaw_right");
+const auto cmdDronePitchUp = sid("cmd_drone_pitch_up");
+const auto cmdDronePitchDown = sid("cmd_drone_pitch_down");
+const auto cmdDroneBankLeft = sid("cmd_drone_bank_left");
+const auto cmdDroneBankRight = sid("cmd_drone_bank_right");
 
 /** 
  * Controls entities with a DroneControllableComponent.
@@ -58,10 +67,13 @@ class DroneControllerProcessor : EntityProcessor {
         //TODO: Rotation
 
         Vector3D newTranslation = Vector3D(0);
+        QuaternionD newRotation = QuaternionD();
 
         bool receivedXTranslation = false;
         bool receivedYTranslation = false;
         bool receivedZTranslation = false;
+
+        bool receivedRotation = false;
 
         messageHandler.receiveMessages(droneChannel, (immutable MagnitudeMessage message) {
             switch (message.id) {
@@ -95,12 +107,42 @@ class DroneControllerProcessor : EntityProcessor {
                 newTranslation.z = message.magnitude;
                 break;
 
+            case cmdDroneYawLeft:
+                receivedRotation = true;
+                newRotation = QuaternionD.createRotation(((2 * PI) / 100) * message.magnitude, Vector3D(0, 1, 0));
+                break;
+
+            case cmdDroneYawRight:
+                receivedRotation = true;
+                newRotation = QuaternionD.createRotation(-(((2 * PI) / 100) * message.magnitude), Vector3D(0, 1, 0));
+                break;
+
+            case cmdDronePitchUp:
+                receivedRotation = true;
+                newRotation = QuaternionD.createRotation(-(((2 * PI) / 100) * message.magnitude), Vector3D(1, 0, 0));
+                break;
+
+            case cmdDronePitchDown:
+                receivedRotation = true;
+                newRotation = QuaternionD.createRotation(((2 * PI) / 100) * message.magnitude, Vector3D(1, 0, 0));
+                break;
+
+            case cmdDroneBankLeft:
+                receivedRotation = true;
+                newRotation = QuaternionD.createRotation(-(((2 * PI) / 100) * message.magnitude), Vector3D(0, 0, 1));
+                break;
+
+            case cmdDroneBankRight:
+                receivedRotation = true;
+                newRotation = QuaternionD.createRotation(((2 * PI) / 100) * message.magnitude, Vector3D(0, 0, 1));
+                break;
+
             default:
                 break;
             }
         });
 
-        if (receivedXTranslation || receivedYTranslation || receivedZTranslation) {
+        if (receivedXTranslation || receivedYTranslation || receivedZTranslation || receivedRotation) {
             foreach (entity; entities) {
                 auto translationSpeedModifier = entity.getFromComponent!DroneControllableComponent(
                     c => c.translationSpeedModifier, 1);
@@ -116,6 +158,12 @@ class DroneControllerProcessor : EntityProcessor {
 
                     if (receivedZTranslation) {
                         c.translation.z = newTranslation.z * translationSpeedModifier;
+                    }
+                });
+
+                entity.maybeWithComponent!SpinningComponent((c) {
+                    if (receivedRotation) {
+                        c.rotation = newRotation;
                     }
                 });
             }
