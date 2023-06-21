8304a4ba72ee7ae59fc4123c6b11c2ccd4f0499e
Mike Bierlee <m.bierlee@lostmoment.com>
2023-06-21 21:00:20 +0300

    Remove getArrayCopy in Queue

--- source/retrograde/std/collections.d
+++ source/retrograde/std/collections.d
@@ -32,6 +32,23 @@ struct Queue(T, size_t chunkSize = defaultChunkSize) {
     private size_t front = 0;
     private size_t rear = 0;
 
+    this(ref return scope Queue!T rhs) {
+        // Force a copy of the items array to be made to prevent the same items array from being shared with rhs.
+        // Without this, this instance's items may be freed up on destruction of rhs.
+        _length = rhs.length;
+        items = rhs.createShuffledResizedQueue(length);
+        front = 0;
+        rear = length;
+        _capacity = getCapacitySizeForItems(length);
+    }
+
+    ~this() {
+        if (items) {
+            free(items);
+            items = null;
+        }
+    }
+
     /**
      * Adds an item to the end of the queue.
      *
@@ -78,21 +95,6 @@ struct Queue(T, size_t chunkSize = defaultChunkSize) {
     }
 
     /**
-     * Returns an exact copy of the internal queue array.
-     *
-     * Params:
-     *  len = Desired length of the returned queue. Make it equal to the queue's capacity to return the complete queue.
-     */
-    T[len] getArrayCopy(size_t len)() nothrow @nogc {
-        T[len] itemsCopy;
-        foreach (i; 0 .. len) {
-            itemsCopy[i] = (items) ? items[i] : T.init;
-        }
-
-        return itemsCopy;
-    }
-
-    /**
      * Clears all items currently in the queue.
      *
      * Items are not deallocated or nullified but stay in the queue as-is. However the queue will be virtually empty.
@@ -121,23 +123,6 @@ struct Queue(T, size_t chunkSize = defaultChunkSize) {
     void deallocate() {
         _length = 0;
         compact();
-    }
-
-    this(ref return scope Queue!T rhs) {
-        // Force a copy of the items array to be made to prevent the same items array from being shared with rhs.
-        // Without this, this instance's items may be freed up on destruction of rhs.
-        _length = rhs.length;
-        items = rhs.createShuffledResizedQueue(length);
-        front = 0;
-        rear = length;
-        _capacity = getCapacitySizeForItems(length);
-    }
-
-    ~this() {
-        if (items) {
-            free(items);
-            items = null;
-        }
     }
 
     private void moveRear() {
@@ -283,72 +268,72 @@ void runQueueTests() {
         // Memory marked as _ used to be enqueued but is not anymore.
 
         queue.enqueue(1); // [1, X, X ,X]
-        assert(queue.getArrayCopy!1 == [1]);
+        assert(queue.items[0] == 1);
         queue.enqueue(2); // [1, 2, X, X]
-        assert(queue.getArrayCopy!2 == [1, 2]);
+        assert(queue.items[0 .. 2] == [1, 2]);
         queue.enqueue(3); // [1, 2, 3, X]
-        assert(queue.getArrayCopy!3 == [1, 2, 3]);
+        assert(queue.items[0 .. 3] == [1, 2, 3]);
         queue.enqueue(4); // [1, 2, 3, 4]
-        assert(queue.getArrayCopy!4 == [1, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [1, 2, 3, 4]);
         assert(queue.capacity == 4);
         assert(queue.length == 4);
         assert(queue.dequeue() == 1); // [_, 2, 3, 4]
-        assert(queue.getArrayCopy!4 == [1, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [1, 2, 3, 4]);
         assert(queue.capacity == 4);
         assert(queue.length == 3);
 
         queue.enqueue(5); // [5, 2, 3, 4]
-        assert(queue.getArrayCopy!4 == [5, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 2, 3, 4]);
         assert(queue.capacity == 4);
         assert(queue.length == 4);
         assert(queue.dequeue() == 2); // [5, _, 3, 4]
-        assert(queue.getArrayCopy!4 == [5, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 2, 3, 4]);
         assert(queue.dequeue() == 3); // [5, _, _, 4]
-        assert(queue.getArrayCopy!4 == [5, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 2, 3, 4]);
         assert(queue.dequeue() == 4); // [5, _, _, _]
-        assert(queue.getArrayCopy!4 == [5, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 2, 3, 4]);
         assert(queue.dequeue() == 5); // [_, _, _, _]
-        assert(queue.getArrayCopy!4 == [5, 2, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 2, 3, 4]);
 
         queue.enqueue(6); // [_, 6, _, _]
-        assert(queue.getArrayCopy!4 == [5, 6, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 6, 3, 4]);
         assert(queue.capacity == 4);
         assert(queue.length == 1);
 
         assert(queue.dequeue() == 6); // [_, _, _, _]
-        assert(queue.getArrayCopy!4 == [5, 6, 3, 4]);
+        assert(queue.items[0 .. 4] == [5, 6, 3, 4]);
         assert(queue.capacity == 4);
         assert(queue.length == 0);
 
         queue.enqueue(7); // [_, _, 7, _]
-        assert(queue.getArrayCopy!4 == [5, 6, 7, 4]);
+        assert(queue.items[0 .. 4] == [5, 6, 7, 4]);
         assert(queue.capacity == 4);
         assert(queue.length == 1);
 
         queue.enqueue(8); // [_, _, 7, 8]
-        assert(queue.getArrayCopy!4 == [5, 6, 7, 8]);
+        assert(queue.items[0 .. 4] == [5, 6, 7, 8]);
         queue.enqueue(9); // [9, _, 7, 8]
-        assert(queue.getArrayCopy!4 == [9, 6, 7, 8]);
+        assert(queue.items[0 .. 4] == [9, 6, 7, 8]);
         queue.enqueue(10); // [9, 10, 7, 8]
-        assert(queue.getArrayCopy!4 == [9, 10, 7, 8]);
+        assert(queue.items[0 .. 4] == [9, 10, 7, 8]);
         assert(queue.capacity == 4);
         assert(queue.length == 4);
 
         // Resize queue that has looped around
         queue.enqueue(11); // [7, 8, 9, 10, 11, X, X, X]
-        assert(queue.getArrayCopy!5 == [7, 8, 9, 10, 11]);
+        assert(queue.items[0 .. 5] == [7, 8, 9, 10, 11]);
 
         assert(queue.capacity == 8);
         assert(queue.length == 5);
 
         assert(queue.dequeue() == 7); // [_, 8, 9, 10, 11, X, X, X]
-        assert(queue.getArrayCopy!5 == [7, 8, 9, 10, 11]);
+        assert(queue.items[0 .. 5] == [7, 8, 9, 10, 11]);
         assert(queue.dequeue() == 8); // [_ ,_, 9, 10, 11, X, X, X]
-        assert(queue.getArrayCopy!5 == [7, 8, 9, 10, 11]);
+        assert(queue.items[0 .. 5] == [7, 8, 9, 10, 11]);
         assert(queue.dequeue() == 9); // [_, _, _, 10, 11, X, X, X]
-        assert(queue.getArrayCopy!5 == [7, 8, 9, 10, 11]);
+        assert(queue.items[0 .. 5] == [7, 8, 9, 10, 11]);
         assert(queue.dequeue() == 10); // [_, _, _, _, 11, X, X, X]
-        assert(queue.getArrayCopy!5 == [7, 8, 9, 10, 11]);
+        assert(queue.items[0 .. 5] == [7, 8, 9, 10, 11]);
         assert(queue.dequeue() == 11); // [_, _, _, _, _, X, X, X]
         assert(queue.capacity == 8);
         assert(queue.length == 0);
