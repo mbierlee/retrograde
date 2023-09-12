64d4652ba8e24a6ad69b2146dff809eca946adfe
Mike Bierlee <m.bierlee@lostmoment.com>
2023-09-13 00:56:08 +0300

    Move implementation of render pass to the pass itself

--- source/retrograde/engine/rendering.d
+++ source/retrograde/engine/rendering.d
@@ -13,7 +13,7 @@ module retrograde.engine.rendering;
 
 import retrograde.std.collections : Array;
 import retrograde.std.memory : SharedPtr;
-import retrograde.std.stringid : sid;
+import retrograde.std.stringid : sid, StringId;
 
 import retrograde.engine.service : entityManager;
 import retrograde.engine.entity : Entity;
@@ -36,12 +36,13 @@ void renderFrame() {
 
     foreach (const ref renderPass; renderPasses) {
         useRenderPassShaderProgram(renderPass);
-        entityManager.forEachEntity((SharedPtr!Entity entity) {
-            if (!entity.ptr.hasComponent(RenderableComponentType)) {
-                return;
-            }
 
-            drawModel(entity, renderPass);
+        //TODO: Optimize? Don't attempt each entity in each pass, but batch them.
+        entityManager.forEachEntity((SharedPtr!Entity entity) {
+            if (entity.ptr.hasComponent(RenderableComponentType) &&
+            entity.ptr.hasComponent(renderPass.componentType)) {
+                renderPass.render(entity, renderPass);
+            }
         });
 
         clearShaderProgram();
@@ -52,13 +53,20 @@ struct RenderPass {
     string passName;
     string vertexShader;
     string fragmentShader;
+    StringId componentType;
+    void delegate(SharedPtr!Entity entity, const ref RenderPass renderPass) render;
+
     uint program;
 }
 
-RenderPass genericRenderPass = RenderPass(
+RenderPass genericModelRenderPass = RenderPass(
     "generic",
-    import("opengles3/generic_vertex.glsl"),
-    import("opengles3/generic_fragment.glsl")
+    import("opengles3/generic_model_vertex.glsl"),
+    import("opengles3/generic_model_fragment.glsl"),
+    ModelComponentType,
+    (SharedPtr!Entity entity, const ref RenderPass renderPass) {
+    drawModel(entity, renderPass);
+}
 );
 
 Array!RenderPass renderPasses;
@@ -79,7 +87,7 @@ struct Color {
 
 private void initRenderPasses() {
     if (renderPasses.length == 0) {
-        renderPasses.add(genericRenderPass);
+        renderPasses.add(genericModelRenderPass);
     }
 
     foreach (ref renderPass; renderPasses) {
