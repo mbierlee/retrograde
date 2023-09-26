4dc17e68f4a109975ca4c0d398d00f13c79f25ae
Mike Bierlee <m.bierlee@lostmoment.com>
2023-09-26 22:38:35 +0300

    Just publicly import native math functions
    The others aren't available in C99 anyway

--- source/retrograde/native/math.d
+++ source/retrograde/native/math.d
@@ -13,42 +13,4 @@ module retrograde.native.math;
 
 version (Native)  :  //
 
-import core.stdc.math : ceil, ceilf, ceill, floor, floorf, floorl, pow, powf, powl;
-
-public import core.stdc.math : sqrt, atan2;
-
-T ceil(T)(T value) {
-    static if (is(T == float)) {
-        return ceilf(value);
-    } else static if (is(T == double)) {
-        return ceil(value);
-    } else static if (is(T == real)) {
-        return ceill(value);
-    } else {
-        static assert(0, "Unsupported type");
-    }
-}
-
-T floor(T)(T value) {
-    static if (is(T == float)) {
-        return floorf(value);
-    } else static if (is(T == double)) {
-        return floor(value);
-    } else static if (is(T == real)) {
-        return floorl(value);
-    } else {
-        static assert(0, "Unsupported type");
-    }
-}
-
-T pow(T)(T base, T exponent) {
-    static if (is(T == float)) {
-        return powf(base, exponent);
-    } else static if (is(T == double)) {
-        return pow(base, exponent);
-    } else static if (is(T == real)) {
-        return powl(base, exponent);
-    } else {
-        static assert(0, "Unsupported type");
-    }
-}
+public import core.stdc.math : sqrt, atan2, ceil, floor, pow;
