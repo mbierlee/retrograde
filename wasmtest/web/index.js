e3fc699dfb11dd1fb6eb1f4809099f7427c5da36
Mike Bierlee <m.bierlee@lostmoment.com>
2023-09-28 23:44:57 +0300

    Rename wasmtest dub project

--- wasmtest/web/index.js
+++ wasmtest/web/index.js
@@ -1,7 +1,7 @@
 import EngineRuntimeModule from "./lib/engine-runtime.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const engineModule = new EngineRuntimeModule("/bin/wasmmemtest.wasm");
+  const engineModule = new EngineRuntimeModule("/bin/wasmtest.wasm");
   engineModule.init().then(() => {
     engineModule.start();
   });
