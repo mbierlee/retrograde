5c68a7913bf4a00927deac7e15789ead5b317fc8
Mike Bierlee <m.bierlee@lostmoment.com>
2024-05-07 14:00:44 +0300

    Split WASM OGL module up into distinct types of declarations

---
+++ source/retrograde/wasm/opengles3/constants.d
@@ -0,0 +1,41 @@
+/**
+ * Retrograde Engine
+ *
+ * Authors:
+ *  Mike Bierlee, m.bierlee@lostmoment.com
+ * Copyright: 2014-2023 Mike Bierlee
+ * License:
+ *  This software is licensed under the terms of the MIT license.
+ *  The full terms of the license can be found in the LICENSE.txt file.
+ */
+
+module retrograde.wasm.opengles3.constants;
+
+version (WebAssembly)  :  //
+version (OpenGLES3)  :  //
+
+// https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Constants
+
+// Clearing buffers
+enum GL_COLOR_BUFFER_BIT = 0x00004000;
+
+// Rendering primitives
+enum GL_TRIANGLES = 0x0004;
+
+// Buffers
+enum GL_STATIC_DRAW = 0x88E4;
+enum GL_ARRAY_BUFFER = 0x8892;
+enum GL_ELEMENT_ARRAY_BUFFER = 0x8893;
+
+// Culling
+enum GL_CULL_FACE = 0x0B44;
+enum GL_FRONT = 0x0404;
+enum GL_BACK = 0x0405;
+enum GL_FRONT_AND_BACK = 0x0408;
+
+// Enabling and disabling
+enum GL_DITHER = 0x0BD0;
+
+// Data types
+enum GL_UNSIGNED_INT = 0x1405;
+enum GL_FLOAT = 0x1406;
--- source/retrograde/wasm/opengles3/package.d
+++ source/retrograde/wasm/opengles3/package.d
@@ -14,43 +14,10 @@ module retrograde.wasm.opengles3;
 version (WebAssembly)  :  //
 version (OpenGLES3)  :  //
 
+public import retrograde.wasm.opengles3.types;
+public import retrograde.wasm.opengles3.constants;
+
 import retrograde.api.opengles3 : glesSetViewport = setViewport;
-
-alias GLuint = uint;
-alias GLint = int;
-alias GLfloat = float;
-alias GLclampf = float; // Use clamp()
-alias GLbool = bool;
-alias GLsizei = int;
-alias GLintptr = int;
-alias GLenum = uint;
-alias GLbitfield = uint;
-
-// https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Constants
-
-// Clearing buffers
-enum GL_COLOR_BUFFER_BIT = 0x00004000;
-
-// Rendering primitives
-enum GL_TRIANGLES = 0x0004;
-
-// Buffers
-enum GL_STATIC_DRAW = 0x88E4;
-enum GL_ARRAY_BUFFER = 0x8892;
-enum GL_ELEMENT_ARRAY_BUFFER = 0x8893;
-
-// Culling
-enum GL_CULL_FACE = 0x0B44;
-enum GL_FRONT = 0x0404;
-enum GL_BACK = 0x0405;
-enum GL_FRONT_AND_BACK = 0x0408;
-
-// Enabling and disabling
-enum GL_DITHER = 0x0BD0;
-
-// Data types
-enum GL_UNSIGNED_INT = 0x1405;
-enum GL_FLOAT = 0x1406;
 
 export extern (C) void resizeCanvasToDisplaySize();
 
