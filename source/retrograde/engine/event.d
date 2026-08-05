6aaaa6f6bf8817cf95980bf270295e2eac26858f
Mike Bierlee <m.bierlee@lostmoment.com>
2026-08-06 00:38:18 +0200

    Add structural event handles
    And unused event data

--- source/retrograde/engine/event.d
+++ source/retrograde/engine/event.d
@@ -13,16 +13,47 @@ module retrograde.engine.event;
 
 import retrograde.std.memory : SharedPtr;
 import retrograde.std.stringid : StringId;
-import retrograde.std.collections : Queue;
+import retrograde.std.collections : Queue, Array;
 import retrograde.std.dlang : CopyConstructors;
 
-alias Magnitude = float;
+version (DoublePrecision) {
+    alias Magnitude = double;
+} else {
+    alias Magnitude = float;
+}
+
+alias EventHandlerFunction = void delegate(ref const Event);
+
 Queue!Event eventQueue;
+Array!EventHandlerFunction eventHandlers;
+
+void processEvents() {
+    Event event;
+    while (eventQueue.tryDequeue(event)) {
+        foreach (handler; eventHandlers) {
+            handler(event);
+        }
+    }
+}
 
 struct Event {
     StringId name;
     Magnitude magnitude;
-    void* eventData;
+    // EventData data1; // TODO: Add back when needed
+}
 
-    mixin CopyConstructors!Event;
+union EventData {
+    ubyte ubData;
+    byte bData;
+    char cData;
+    wchar wcData;
+    uint uiData;
+    int iData;
+    float fData;
+    dchar dcData;
+
+    version (LargeEventData)  :  //
+    ulong ulData;
+    long lData;
+    double dData;
 }
